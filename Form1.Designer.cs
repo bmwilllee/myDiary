6fc57afe57d121682e0defa12e0fdf8a145609b7
will lee <986852442@qq.com>
2017-10-18 00:26:03 +0800

    commit the basic code file

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace exercise2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.EditBox = new System.Windows.Forms.RichTextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.TitleBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NewDiary = new System.Windows.Forms.Button();
+            this.TimeBox = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 19;
+            this.listBox.Location = new System.Drawing.Point(12, 40);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(152, 327);
+            this.listBox.TabIndex = 1;
+            this.listBox.Click += new System.EventHandler(this.ListBox_Click);
+            // 
+            // EditBox
+            // 
+            this.EditBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.EditBox.Location = new System.Drawing.Point(170, 40);
+            this.EditBox.Margin = new System.Windows.Forms.Padding(0);
+            this.EditBox.Name = "EditBox";
+            this.EditBox.Size = new System.Drawing.Size(403, 400);
+            this.EditBox.TabIndex = 2;
+            this.EditBox.Text = "";
+            // 
+            // Save
+            // 
+            this.Save.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save.Location = new System.Drawing.Point(47, 392);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 29);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // TitleBox
+            // 
+            this.TitleBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleBox.Location = new System.Drawing.Point(170, 12);
+            this.TitleBox.Name = "TitleBox";
+            this.TitleBox.Size = new System.Drawing.Size(197, 22);
+            this.TitleBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Title :";
+            // 
+            // NewDiary
+            // 
+            this.NewDiary.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.NewDiary.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewDiary.Location = new System.Drawing.Point(12, 3);
+            this.NewDiary.Name = "NewDiary";
+            this.NewDiary.Size = new System.Drawing.Size(75, 33);
+            this.NewDiary.TabIndex = 7;
+            this.NewDiary.Text = "New";
+            this.NewDiary.UseVisualStyleBackColor = false;
+            this.NewDiary.Click += new System.EventHandler(this.NewDiary_Click);
+            // 
+            // TimeBox
+            // 
+            this.TimeBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.TimeBox.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeBox.Location = new System.Drawing.Point(386, 10);
+            this.TimeBox.Name = "TimeBox";
+            this.TimeBox.Size = new System.Drawing.Size(187, 23);
+            this.TimeBox.TabIndex = 20;
+            this.TimeBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 452);
+            this.Controls.Add(this.TimeBox);
+            this.Controls.Add(this.NewDiary);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TitleBox);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.EditBox);
+            this.Controls.Add(this.listBox);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "NotePad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.RichTextBox EditBox;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox TitleBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button NewDiary;
+        private System.Windows.Forms.Label TimeBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Timers;
+
+namespace exercise2
+{
+    public partial class Form1 : Form
+    {
+        public string titleChoose;
+        public string timeChoose;
+        public string textEdit;
+        string Currentpath = System.Windows.Forms.Application.StartupPath; // obtain the current path;
+        //string Currentpath = @"C:\Users\weigirl\Desktop";
+        string target = @"\myDiary"; // the new directory name
+
+        public class Diary // each diary as a object of Diary class
+        {
+            public string time;
+            public string text;
+            public string title;
+            public string path;
+
+            public Diary(string title) // constructor
+            {
+                this.title = title;
+            }
+            public void Delete(string path) // delete current choosen diary
+            {
+                File.Delete(path);
+            }
+
+            public void Save(string path) // save current edit or save new diary
+            {
+                if(path.IndexOf(".txt") != 1)
+                {
+                    path += ".txt";
+                }
+                if (File.Exists(path)) // judge weither this file is exist
+                {
+                    this.Delete(path);
+                }
+                FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write); // create new file and write
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(this.text);
+                sw.Close();
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void listShow()
+        {
+            string path = string.Concat(Currentpath + target);
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            foreach (FileInfo NextFile in directoryInfo.GetFiles())
+            {
+                listBox.Items.Add(NextFile.Name);  // traverse the all files and show on the listBox
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = string.Concat(Currentpath + target);
+            if (!Directory.Exists(path)) // judge weither there has such directory
+            {
+                Directory.CreateDirectory(path);
+            }
+            listShow();
+
+        }
+
+        public string OpenFile(string fileName)
+        {
+            string readText = "\t";
+            StreamReader sr = new StreamReader(fileName);
+            string line;
+            while((line = sr.ReadLine()) != null){
+                Console.WriteLine(line.ToString());
+                readText += string.Concat(string.Concat(line, "\r\n"));
+            }
+            return readText;
+        }
+
+        private void NewDiary_Click(object sender, EventArgs e) // clean the title and editBox
+        {
+            TitleBox.Text = "";
+            EditBox.Text = "";
+            timer1.Start();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            titleChoose = TitleBox.Text;
+            Diary diary = new Diary(titleChoose);
+            diary.text = EditBox.Text;
+            diary.Save(string.Concat(Currentpath, target, @"\", titleChoose));
+        }
+
+        //private void Delete_Click(object sender, EventArgs e)
+        //{
+        //    titleChoose = TitleBox.Text;
+        //    Diary diary = new Diary(titleChoose);
+        //    diary.Delete(string.Concat(Currentpath, target, @"\", titleChoose));
+        //}
+
+        private void Timer1_Tick(object sender, EventArgs e) // Show the current time
+        {
+            TimeBox.Text = DateTime.Now.ToString();
+        }
+
+        private void ListBox_Click(object sender, EventArgs e) // When click the saved diaries
+        {
+            string fileName = listBox.SelectedItem.ToString();
+            Diary diary = new Diary(fileName);
+            diary.text = OpenFile(string.Concat(Currentpath, target, @"\", fileName));
+            TitleBox.Text = fileName;
+            EditBox.Text = diary.text;
+            FileInfo fi = new FileInfo(string.Concat(Currentpath, target, @"\", fileName));
+            TimeBox.Text = fi.CreationTime.ToString();
+            timer1.Stop();
+        }
+
+    }
+}
